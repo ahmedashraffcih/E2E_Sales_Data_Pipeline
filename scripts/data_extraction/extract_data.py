18f958f489b05910aff21806c30628970754d4ca
ahmedashraffcih <ahmedashraffcih@gmail.com>
2024-05-21 19:08:20 +0400

    UPDATE: bronze layer and logic

--- scripts/data_extraction/extract_data.py
+++ scripts/data_extraction/extract_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from datetime import datetime
-from utils.utils import format_data, extract_users_api, export_to_json
+from utils.utils import format_users_data, extract_users_api, export_to_json, extract_weather_api
 
 def extract_users_data_json():
     # Export data to bronze layer
@@ -9,9 +9,22 @@ def extract_users_data_json():
 
 def extract_users_data_csv():
     users_data = extract_users_api()
-    formatted_data = [format_data(user) for user in users_data]
+    formatted_data = [format_users_data(user) for user in users_data]
     users_df = pd.DataFrame(formatted_data)
     users_df.to_csv('./datalake/bronze/users.csv',index=False)
     print('users.csv file has been exported to ./datalake/bronze/users.csv')
 
-extract_users_data_csv()
+# def extract_users_df():
+#     users_data = extract_users_api()
+#     formatted_data = [format_users_data(user) for user in users_data]
+#     return pd.DataFrame(formatted_data)
+
+def extract_weather_data_csv():
+    # Export data to bronze layer
+    users_data = extract_users_api()
+    formatted_data = [format_users_data(user) for user in users_data]
+    users_df = pd.DataFrame(formatted_data)
+    location_df = users_df[['id', 'lat', 'lng']]
+    locationlist = list(location_df.values)
+    weather_df = pd.DataFrame(extract_weather_api(locationlist))
+    weather_df.to_csv('./datalake/bronze/weather.csv',index=False)
--- scripts/data_extraction/utils/utils.py
+++ scripts/data_extraction/utils/utils.py
@@ -1,24 +1,57 @@
 import requests
 import os
 import json
-import pandas as pd
-from datetime import datetime
+from dotenv import load_dotenv
+
+def load_api_key():
+    load_dotenv()  # Load variables from .env file
+    openweathermap_api_key = os.getenv("OPENWEATHERMAP_API_KEY")
+    return openweathermap_api_key
 
 def extract_users_api():
     response = requests.get('https://jsonplaceholder.typicode.com/users')
     if response.status_code == 200:
-        users_data = response.json()
-        return users_data
+        return response.json()
     else:
-        r = response
-        message =  "HTTP %i - %s, Message %s" % (r.status_code, r.reason, r.text)
-        print(message)
+        print(f"Error fetching weather data: HTTP {response.status_code} - {response.reason}")
 
-def format_data(res):
+
+def extract_weather_api(locationlist):
+    weather_list = []
+    for location in locationlist:
+        url = "http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid={2}"\
+            .format(location[1], location[2],load_api_key())
+        response = requests.get(url)
+        if response.status_code == 200:
+            res = response.json()
+        else:
+            r = response
+            message =  "HTTP %i - %s, Message %s" % (r.status_code, r.reason, r.text)
+            print(message)
+        data = {}
+        data['id'] = location[0]
+        data['lat']= res['coord']['lat']
+        data['lng']= res['coord']['lon']
+        data['weather']= res['weather'][0]['main']
+        data['description']= res['weather'][0]['description'] 
+        data['temp']= res['main']['temp']
+        data['pressure']= res['main']['pressure']
+        data['humidity']= res['main']['humidity']
+        data['grnd_level']= res['main']['grnd_level']
+        data['sea_level']= res['main']['sea_level']
+        data['wind_speed']= res['wind']['speed']
+        data['wind_deg']= res['wind']['deg']
+        data['wind_gust']= res['wind']['gust']
+        data['pressure']= res['main']['pressure']
+        weather_list.append(data)
+    return weather_list
+
+def format_users_data(res):
     data = {}
     # Flat values
     data['id']= res['id']
     data['name']= res['name']
+    data['username']= res['username']
     data['email']= res['email']
     data['phone']= res['phone']
     data['website']= res['website']
